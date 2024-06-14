c90f5eea7d15c4474854a39d5afb241bba512b3f
YashasviSingh007 <syash2836@gmail.com>
2024-06-14 17:04:36 +0530

    Update OYO DATABASE.sql

--- OYO DATABASE.sql
+++ OYO DATABASE.sql
@@ -79,3 +79,28 @@ CREATE TABLE Service_Employee (
     FOREIGN KEY (service_id) REFERENCES Service(service_id),
     FOREIGN KEY (employee_id) REFERENCES Employee(employee_id)
 );
+
+
+
+
+-- Bookings Table by Yashasvi Singh
+CREATE TABLE Bookings (
+    booking_id INT PRIMARY KEY ,
+    user_id INT,
+    room_id INT,
+    check_in DATE NOT NULL,
+    check_out DATE NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (room_id) REFERENCES Rooms(room_id)
+);
+
+-- Payments Table by Yashasvi Singh
+CREATE TABLE Payments (
+    payment_id INT PRIMARY KEY,
+    booking_id INT,
+    amount DECIMAL(10, 2) NOT NULL,
+    payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    payment_method VARCHAR(50) NOT NULL,
+    FOREIGN KEY (booking_id) REFERENCES Bookings(booking_id)
+);
